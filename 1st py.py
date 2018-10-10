9973c520e097f8ce2dbada405719393013e6c980
Dosc <dosc_tongji@163.com>
2018-10-10 11:47:02 +0800

    Update 1st py.py

--- 1st py.py
+++ 1st py.py
@@ -1,6 +1,50 @@
 import time
 
 
-print("Hello World!")
+def fib1(n):
+    """
+    fibonocci without dict
+    """
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fib1(n-1) + fib1(n-2)
 
 
+known = {0: 0, 1: 1}
+
+
+def fib2(n):
+    """
+    fibonocci with dict
+
+    """
+    if n in known:
+        return known[n]
+
+    res = fib2(n-1) + fib2(n-2)
+    known[n] = res
+    return res
+
+
+"""
+runtime of fibonocci  with/without memo
+
+
+time_start = time.clock()
+
+print(fib1(30))
+time1 = time.clock()
+print('time without dict:', time1 - time_start)
+
+print(fib2(30))
+time2 = time.clock()
+print('time with dict:', time2 - time1)
+
+"""
+
+a = ('a',)
+b = ('b')
+print(type(a), '\n', type(b))
